042b00c1f379ce532473c817c65eccdfdcffbd63
sven-gerlach <svengerlach@protonmail.com>
2023-02-20 13:59:50 -0500

    Remove boolean coercion from field over

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -3,7 +3,7 @@ const ui = require('./ui')
 
 const Game = function (cells, over, _id, owner, createdAt, updatedAt, symbol, turn, opponent, aiDifficulty) {
   this.cells = cells
-  this.over = Boolean(over)
+  this.over = over
   this.id = _id
   this.player = symbol
   this.owner = owner
